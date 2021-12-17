283aed533a399727af14c6af866dda3bc168f1e1
poom <poom..@Pooms-MacBook-Pro.local>
2021-12-17 17:02:49 +0700

    this commit is add env config and graceful shut down

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.7
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Poom5741/learn_go_api/auth"
 	"github.com/Poom5741/learn_go_api/todo"
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
@@ -10,6 +20,11 @@ import (
 )
 
 func main() {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		log.Println("please consider environment variables: %s", err)
+
+	}
 
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
@@ -24,10 +39,36 @@ func main() {
 		})
 	})
 
-	r.GET("/tokenz", auth.AccessToken("==signature=="))
-	protected := r.Group("", auth.Protect([]byte("==signature==")))
+	r.GET("/tokenz", auth.AccessToken(os.Getenv("SIGN")))
+	protected := r.Group("", auth.Protect([]byte(os.Getenv("SIGN"))))
 	handler := todo.NewTodoHandler(db)
 	protected.POST("/todos", handler.NewTask)
 
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	s := &http.Server{
+		Addr:           ":" + os.Getenv("PORT"),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 10,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s \n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	fmt.Println("Shutting down gracefully, press control c again to force")
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
 	r.Run()
 }
